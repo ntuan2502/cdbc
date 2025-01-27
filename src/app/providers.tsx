5e81328b7730b4e5753c826a23ee31c3a819297b
Tuan Nguyen <ntuan.2502@gmail.com>
2025-01-27 09:24:20 +0700

    add @vercel/analytics @vercel/speed-insights

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,5 +1,7 @@
 // app/providers.tsx
 "use client";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 import { HeroUIProvider } from "@heroui/react";
 import { ThemeProvider } from "./theme-context";
@@ -7,7 +9,11 @@ import { ThemeProvider } from "./theme-context";
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <HeroUIProvider>
-      <ThemeProvider>{children}</ThemeProvider>
+      <ThemeProvider>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </ThemeProvider>
     </HeroUIProvider>
   );
 }
